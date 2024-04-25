29467747ea8f118a15d8b0d6b9554a456a49cbb0
StudentNSTU <160150922+StudentNSTU@users.noreply.github.com>
2024-04-25 15:53:03 +0700

    Add files via upload

---
+++ block_1/Code/WindowsFormsApp1/src/tokensList.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace WindowsFormsApp1.src
+{
+    internal class tokensList
+    {
+        public Dictionary<string, TokenType> tokenTypeList = new Dictionary<string, TokenType>
+        {
+            { "REGEXP", new TokenType("REGEXP", "\\d{5}(-\\d{4})?") },
+            { "ANY", new TokenType("ANY", ".*") }
+        };
+    }
+}
